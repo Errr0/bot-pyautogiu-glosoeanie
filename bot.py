0293fcb57f8b584c9bda601d65e95c4bbb89fe5a
Errr0 <119783076+Errr0@users.noreply.github.com>
2024-11-27 14:53:58 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,58 @@
+import pyautogui
+
+
+
+
+
+
+
+
+
+# print(pyautogui.position())
+# print(pyautogui.size())
+# pyautogui.click(x=1560, y=430)
+while(1):
+# for x in range(1):
+    pyautogui.click(x=1920/4, y=1080/4)
+    pyautogui.PAUSE = 0.2
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.hotkey('return')
+    pyautogui.click(x=1920/4, y=(1080/4)*3)
+    pyautogui.PAUSE = 0.2
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.hotkey('return')
+    pyautogui.click(x=(1920/4)*3, y=1080/4)
+    pyautogui.PAUSE = 0.2
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.hotkey('return')
+    pyautogui.click(x=(1920/4)*3, y=(1080/4)*3)
+    pyautogui.PAUSE = 0.2
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.hotkey('return')
+    # pyautogui.PAUSE = 0.9
+    # pyautogui.hotkey('ctrl', 'v')
+    # pyautogui.PAUSE = 0.1
+    # pyautogui.hotkey('return')
+
+
+
+
+# pyautogui.hotkey('ctrl', 'shift', 'n')
+# pyautogui.PAUSE = 0.5
+# pyautogui.click(x=541, y=62)
+# pyautogui.PAUSE = 0.5
+# pyautogui.typewrite('https://aktywisci.oblicza-historii.pl/2023/12/31/jan-wedel/\n', interval=0.05)
+# pyautogui.hotkey('ctrl', 'shift', 'i')
+# pyautogui.PAUSE = 0.5
+# pyautogui.click(x=1545, y=165)
+# pyautogui.PAUSE = 0.5
+# pyautogui.click(x=1560, y=430)
+# pyautogui.PAUSE = 0.5
+# pyautogui.typewrite('allow pasting\n', interval=0.05)
+# pyautogui.PAUSE = 0.5
+
+#print(pyautogui.position())
+#print(pyautogui.size())
+#pyautogui.moveTo()
+#pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
+#pyautogui.hotkey('ctrl', 'v')
